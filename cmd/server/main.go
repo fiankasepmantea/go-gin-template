2c91f1ef307e0c625e90df95d44c3312910e62c6
fiankasepmantea <fianet341@gmail.com>
2026-03-16 14:14:27 +0700

    init

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,20 +2,36 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"github.com/fiankasepman/go-gin-template/configs"
 	"github.com/fiankasepman/go-gin-template/internal/database"
+	"github.com/fiankasepman/go-gin-template/internal/middleware"
+	userModule "github.com/fiankasepman/go-gin-template/internal/modules/user"
 )
 
 func main() {
 
+	configs.LoadEnv()
+
 	database.Connect()
 	database.RunMigrations()
+	database.SeedAll()
 
 	r := gin.Default()
 
+	r.Use(middleware.RBACMiddleware())
+
+	db := database.DB
+
+	userRepo := userModule.NewRepository(db)
+	userService := userModule.NewService(userRepo)
+	userHandler := userModule.NewHandler(userService)
+
+	r.GET("/users", userHandler.GetAll)
+	r.POST("/login", userHandler.Login)
+
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "server running",
-		})
+		c.JSON(200, gin.H{"message": "server running"})
 	})
 
 	r.Run(":8080")
--- configs/env.go
+++ configs/env.go
@@ -1,1 +1,14 @@
 package configs
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("no .env file")
+	}
+}
